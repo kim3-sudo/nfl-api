5876c16991350eb2a787e46993106ee8debb24b3
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2023-07-12 16:55:43 -0400

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "nflapi");
-define("DB_PASSWORD", "PhilanderChase");
+define("DB_PASSWORD", "thisisnotavalidpasswordthatcanbeusedhere");
 define("DB_SCHEMA", "nfl");
